3b47203768934b2991be219192a7c8c9c166ef4b
elhachimi <wael@unik.ma>
2014-12-17 00:14:15 +0000

    add friends/new controller

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    save: function() {
+      console.log('+-- save action bubbled up to the friends new route');
+
+      return true;
+    },
+
+    cancel: function () {
+      console.log('+-- cancel action bubbled up to the friends new route');
+
+      return true;
+    }
+  }
 });
